8a864fe672231a16c83642c0e6afbc2640c0bdfe
SVHS Student <svhsstudent@SVUSD.ME>
2018-12-11 09:41:42 -0800

    added new files

---
+++ Assets/Navigation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Navigation : MonoBehaviour {
+
+	public Transform dp; 
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.GetComponent<UnityEngine.AI.NavMeshAgent>().destination = dp.position;
+	}
+}
